37df9e70e1e309ecb260c18f836e438418bc3a5e
yaman1337 <yaman.sarabariya@gmail.com>
2023-06-08 03:14:47 +0545

    [refactor]: refactor code

--- src/pages/Dashboard/Contributions/Add Places/General/generalDetails.jsx
+++ src/pages/Dashboard/Contributions/Add Places/General/generalDetails.jsx
@@ -1,118 +1,120 @@
-import {
-    Form,
-    Image,
-    Input,
-    Select,
-    Upload,
-  } from '@arco-design/web-react';
-import { IconDelete } from '@arco-design/web-react/icon';
+import { Form, Image, Input, Select, Upload } from "@arco-design/web-react";
+import { IconDelete } from "@arco-design/web-react/icon";
 
 // importing styles
-import "./generalDetails.css"
+import "./generalDetails.css";
 
 const FormItem = Form.Item;
 const KeywordOptions = [
   {
-    label: 'Historic',
-    value: 'historic',
+    label: "Historic",
+    value: "historic",
   },
   {
-    label: 'Religious',
-    value: 'religious',
+    label: "Religious",
+    value: "religious",
   },
   {
-    label: 'Park',
-    value: 'park',
+    label: "Park",
+    value: "park",
   },
   {
-    label: 'Nature',
-    value: 'nature',
+    label: "Nature",
+    value: "nature",
   },
   {
-    label: 'Others',
-    value: 'others',
+    label: "Others",
+    value: "others",
   },
 ];
 
 const inputStyle = {
-    minWidth: '250px',
-    width: '100%',
-    maxWidth: '400px'}
+  minWidth: "250px",
+  width: "100%",
+  maxWidth: "400px",
+};
 
-const GeneralDetails = ({state}) => {
+const GeneralDetails = ({ state }) => {
+
   const renderUploadList = (filesList, props) => (
-    <div style={{ display: 'flex', gap: '1em 1em' }} >
+    <div style={{ display: "flex", gap: "1em 1em" }}>
       {filesList.map((file) => {
         const url = file.url || URL.createObjectURL(file.originFile);
         return (
-          <Image 
+          <Image
             width={80}
             height={80}
             src={url}
-            alt='img'
+            alt="img"
             actions={[
               <button
-                key='1'
-                className='image-demo-action-item'
+                key="1"
+                className="image-demo-action-item"
                 style={{
-                  background: 'transparent', 
-                  color: 'white', 
-                  outline: 'none', 
-                  border: 'none', 
-                  fontSize: '18px'
+                  background: "transparent",
+                  color: "white",
+                  outline: "none",
+                  border: "none",
+                  fontSize: "18px",
                 }}
                 onClick={() => {
                   props.onRemove(file);
                 }}
               >
                 <IconDelete />
-              </button>
+              </button>,
             ]}
           />
         );
       })}
     </div>
-  )
+  );
 
-  return(
-        <>
-        <div className="left item">
-            <Form.Item
-              style={{width: 'auto'}}
-              label='Place Images'
-              field='images'
-              triggerPropName='fileList'
-              rules={[{ required: true }]}
-            >
-              <Upload
-                onChange={e => state.setImageFiles(e)}
-                listType='picture-card'
-                multiple
-                name='files'
-                renderUploadList={renderUploadList}
-                action='/'
-              />
-            </Form.Item>
-    
-            <FormItem label='Title' field='title' tooltip={<div>Title is the name of place </div>} rules={[{ required: true }]}>
-              <Input 
-              onChange={(e) => state.setTitle(e)}
-              style={inputStyle} 
-              placeholder='please enter your name' />
-            </FormItem>
-    
-            <FormItem label='Post' field='post' rules={[{ required: true }]}>
-                <Select
-                  onChange={(e) => state.setKeyWord(e)}
-                  style={inputStyle}
-                  placeholder='please select'
-                  options={KeywordOptions}
-                  allowClear
-                />
-            </FormItem>
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="left item">
+        <Form.Item
+          style={{ width: "auto" }}
+          label="Place Images"
+          field="images"
+          triggerPropName="fileList"
+          rules={[{ required: true }]}
+        >
+          <Upload
+            onChange={(e) => state.setImageFiles(e)}
+            listType="picture-card"
+            multiple
+            name="files"
+            renderUploadList={renderUploadList}
+            action="/"
+          />
+        </Form.Item>
+
+        <FormItem
+          label="Title"
+          field="title"
+          tooltip={<div>Title is the name of place </div>}
+          rules={[{ required: true }]}
+        >
+          <Input
+            onChange={(e) => state.setTitle(e)}
+            style={inputStyle}
+            placeholder="please enter your name"
+          />
+        </FormItem>
+
+        <FormItem label="Post" field="post" rules={[{ required: true }]}>
+          <Select
+            onChange={(e) => state.setKeyWord(e)}
+            style={inputStyle}
+            placeholder="please select"
+            options={KeywordOptions}
+            allowClear
+          />
+        </FormItem>
+      </div>
+    </>
+  );
+};
 
 export default GeneralDetails;
--- src/pages/Dashboard/Contributions/Add Places/renderSteps.jsx
+++ src/pages/Dashboard/Contributions/Add Places/renderSteps.jsx
@@ -1,53 +1,52 @@
-import { Button } from '@arco-design/web-react';
-import { IconLeft, IconRight } from '@arco-design/web-react/icon';
+import { Button } from "@arco-design/web-react";
+import { IconLeft, IconRight } from "@arco-design/web-react/icon";
 
-import GeneralDetails from './General/generalDetails';
-import LocationDetails from './Location/LocationDetails';
-import PlaceDescription from './Description/PlaceDescription';
+import GeneralDetails from "./General/generalDetails";
+import LocationDetails from "./Location/LocationDetails";
+import PlaceDescription from "./Description/PlaceDescription";
 
-const RenderContent = ({state}) => {
-    return (
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          textAlign: 'center',
-          background: 'var(--color-bg-2)',
-          color: '#C2C7CC',
-        }}
-      >
-        {
-            state.current === 1 ? 
-            (<GeneralDetails state={state} />)
-            : state.current === 2 ? (
-              <LocationDetails state={state} />
-            ) : (
-              <PlaceDescription state={state} />
-            )
-        }
+const RenderContent = ({ state }) => {
 
-        <div style={{textAlign: 'left'}}>
-          <Button
-            type='secondary'
-            disabled={state.current <= 1}
-            onClick={() => state.setCurrent(state.current - 1)}
-            style={{ paddingLeft: 8 }}
-          >
-            <IconLeft />
-            Back
-          </Button>
-          <Button
-            disabled={state.current >= 3}
-            onClick={() => state.setCurrent(state.current + 1)}
-            style={{ marginLeft: 20, paddingRight: 8 }}
-            type='primary'
-          >
-            Next
-            <IconRight />
-          </Button>
-        </div>
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        textAlign: "center",
+        background: "var(--color-bg-2)",
+        color: "#C2C7CC",
+      }}
+    >
+      {state.current === 1 ? (
+        <GeneralDetails state={state} />
+      ) : state.current === 2 ? (
+        <LocationDetails state={state} />
+      ) : (
+        <PlaceDescription state={state} />
+      )}
+
+      <div style={{ textAlign: "left" }}>
+        <Button
+          type="secondary"
+          disabled={state.current <= 1}
+          onClick={() => state.setCurrent(state.current - 1)}
+          style={{ paddingLeft: 8 }}
+        >
+          <IconLeft />
+          Back
+        </Button>
+        <Button
+          disabled={state.current >= 3}
+          onClick={() => state.setCurrent(state.current + 1)}
+          style={{ marginLeft: 20, paddingRight: 8 }}
+          type="primary"
+        >
+          Next
+          <IconRight />
+        </Button>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default RenderContent;
