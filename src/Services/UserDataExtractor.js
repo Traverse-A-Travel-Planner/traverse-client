cd6c87ff55a981c1cda53cf3a990e7962aec0867
yaman1337 <yaman.sarabariya@gmail.com>
2023-06-12 10:58:16 +0545

    [fix]: user extractor order fixed

--- src/Services/UserDataExtractor.js
+++ src/Services/UserDataExtractor.js
@@ -3,25 +3,25 @@ import appwriteClient from "./appwriteClient";
 
 async function userDataExtractor(data) {
   try {
-    if (data.length <= 0 || Object.keys(data).length <= 0){
+    if (data.length <= 0 || Object.keys(data).length <= 0) {
       return {
         success: false,
-        message: "Data not present"
-      }
+        message: "Data not present",
+      };
     }
 
     let userID;
     let dataType = "";
 
-    if (data.author_id){
-      dataType = "object"
+    if (data.author_id) {
+      dataType = "object";
       userID = [data.author_id];
     } else {
-      dataType = "array"
-      userID = data.map(item => item.author_id);
+      dataType = "array";
+      userID = data.map((item) => item.author_id);
     }
 
-    let executionResponse = []
+    let executionResponse = [];
 
     const functions = new Functions(appwriteClient);
     executionResponse = await functions.createExecution(
@@ -36,33 +36,42 @@ async function userDataExtractor(data) {
     if (!executionResponse || !executionResponse.success) {
       return {
         success: false,
-        message: "Internal server error occured while fetching place details."
-      }
+        message: "Internal server error occured while fetching place details.",
+      };
     }
 
-    if (dataType === "object"){
+    if (dataType === "object") {
       return {
         success: true,
-        data: { ...data, name: executionResponse.userDetails.users[0].name }
-      }
+        data: { ...data, name: executionResponse.userDetails.users[0].name },
+      };
     } else if (dataType === "array") {
-      let finalData = executionResponse.userDetails.users?.map(
-        (item, idx) => {
-          let currentReview = data[idx];
-          return { ...currentReview, name: item.name };
+      let arrayOfUsers = executionResponse.userDetails.users;
+
+      let finalData = [];
+
+      for (let i = 0; i < data.length; i++) {
+        let currentReview = data[i];
+
+        for (let j = 0; j < arrayOfUsers.length; j++) {
+          let item = arrayOfUsers[j];
+          console.log(item.$id, currentReview.author_id);
+          if (item.$id === currentReview.author_id) {
+            finalData.push({ ...currentReview, name: item.name });
+          }
         }
-      )
+      }
 
       return {
         success: true,
-        data: finalData
-      }
+        data: finalData,
+      };
     }
   } catch (error) {
     return {
       success: false,
-      message: "Something went wrong"
-    }
+      message: "Something went wrong",
+    };
   }
 }
 
