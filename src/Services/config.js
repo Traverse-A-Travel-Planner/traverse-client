56acf14ccfb89bc33e34c1a6f610e48f4a4a8ecd
Yaman Sarabariya <63846784+yaman1337@users.noreply.github.com>
2023-06-14 22:27:25 +0545

    Update config.js

--- src/Services/config.js
+++ src/Services/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  backendUrl: "http://localhost:9000",
   appwriteEndPoint: "https://cloud.appwrite.io/v1",
   appwriteProjectId: "64777ba0910c827a975b",
   databaseId: "traverse",
